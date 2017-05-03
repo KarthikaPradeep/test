5a0e337cc1e859b43cc2641efd4a1e44662f3b93
nishijayaraj <nishijayaraj@gmail.com>
2017-05-03 14:48:15 +0530

    first commit

---
+++ config.sh
@@ -0,0 +1,8 @@
+echo "user:" $USER
+echo "login:" $LOGNAME
+echo "shell:" $SHELL
+echo "home directory:" $HOME
+echo "operating system type:" $OSTYPE
+echo "path:" $PATH
+echo "currently logged users:" `who | wc`
+echo "current working directory:" $PWD
